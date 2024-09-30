517b520d438523621fa6c5cd0f19047f8a730cb4
DEDSEC-WEAPON\minh <mnguyenngoc69@gmail.com>
2024-09-30 13:58:48 +0700

    Thêm mới nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,4 +1,5 @@
 const Role = require("../../models/role.model");
+const systemConfig = require("../../config/system");
 
 module.exports.index = async (req, res) => {
   const records = await Role.find({
@@ -10,4 +11,18 @@ module.exports.index = async (req, res) => {
     records: records,
   }
   );
+}
+
+module.exports.create = async (req, res) => {
+  res.render("admin/pages/roles/create", {
+    pageTitle: "Trang thêm nhóm quyền",
+  })
+}
+
+module.exports.createPOST = async (req, res) => {
+  const role = new Role(req.body);
+  await role.save();
+
+  res.redirect(`/${systemConfig.prefixAdmin}/roles`)
+  
 }
