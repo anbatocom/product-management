b56dafc69d7c7c60b04bb5a98df7652cdbfdb75e
DEDSEC-WEAPON\minh <mnguyenngoc69@gmail.com>
2024-10-07 04:58:27 +0700

    Middleware để cho các route thêm tính private

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -5,14 +5,17 @@ const systemConfig = require("../../config/system.js")
 const roleRouter = require("./role.route")
 const accountRouter = require("./account.route")
 const authRouter = require("./auth.route")
+
+const authMiddleware = require("../../middlewares/admin/auth.middleware.js")
+
 module.exports.index = (app) => {
 
   const PATH_ADMIN = `/${systemConfig.prefixAdmin}`;
 
-  app.use(`${PATH_ADMIN}/dashboard`, dashboardRoute)
-  app.use(`${PATH_ADMIN}/products`, productRoute)
-  app.use(`${PATH_ADMIN}/product-category`, productCategoryRoute)
-  app.use(`${PATH_ADMIN}/roles`, roleRouter)
-  app.use(`${PATH_ADMIN}/accounts`, accountRouter)
+  app.use(`${PATH_ADMIN}/dashboard`,authMiddleware.requireAuth, dashboardRoute)
+  app.use(`${PATH_ADMIN}/products`,authMiddleware.requireAuth, productRoute)
+  app.use(`${PATH_ADMIN}/product-category`,authMiddleware.requireAuth, productCategoryRoute)
+  app.use(`${PATH_ADMIN}/roles`,authMiddleware.requireAuth, roleRouter)
+  app.use(`${PATH_ADMIN}/accounts`,authMiddleware.requireAuth, accountRouter)
   app.use(`${PATH_ADMIN}/auth`, authRouter)
 }
