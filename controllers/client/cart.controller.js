d42ca56fe094df9ffde6067880506e0b2d60c680
DEDSEC-WEAPON\minh <mnguyenngoc69@gmail.com>
2024-10-16 17:21:14 +0700

    Lam trang gio hang

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,16 +1,58 @@
 const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+
+  const cartID = req.cookies.cartId;
+
+  const cart = await Cart.findOne({
+    _id: cartID
+  })
+
+  const products = cart.products;
+  
+  let total = 0;
+
+  for (const item of products) {
+    const infoItem = await Product.findOne({
+      _id: item.productID,
+      deleted: false,
+      status: "active"
+    })
+
+    item.thumbnail = infoItem.thumbnail
+    item.title = infoItem.title
+    item.slug = infoItem.slug
+    item.priceNew = infoItem.price
+    
+    if (infoItem.discountPercentage > 0) {
+      item.priceNew = (1 - infoItem.discountPercentage/100) * infoItem.price
+      item.priceNew = item.priceNew.toFixed(0)
+    }
+    item.total = item.priceNew * item.quantity
+    total += item.total;
+  }
+  
+  
+  res.render("client/pages/cart/index", {
+    pageTitle: "Giỏ hàng",
+    products: products,
+    total: total,
+  })
+}
+
 module.exports.addPOST = async (req, res) => {
   const cartID = req.cookies.cartId
 
   const cart = await Cart.findOne({
     _id: cartID
   })
-  
+
   const products = cart.products;
 
   const existProduct = products.find((item => item.productID == req.params.id))
 
-  if(existProduct) {
+  if (existProduct) {
     existProduct.quantity = existProduct.quantity + parseInt(req.body.quantity)
 
   } else {
@@ -21,13 +63,13 @@ module.exports.addPOST = async (req, res) => {
     products.push(product); // them vao mang products 
   }
 
-  
+
   await Cart.updateOne({
     _id: cartID
-  },{
+  }, {
     products: products
   })
-  
+
   req.flash("success", "Đã thêm sản phẩm vào giỏ hàng")
   res.redirect("back")
 }
